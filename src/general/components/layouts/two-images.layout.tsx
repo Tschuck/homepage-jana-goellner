9b8f992fe0042e02742b5ad33a80ca9377cf94b5
Tobias Winkler <7114819+Tschuck@users.noreply.github.com>
2025-08-26 12:33:24 +0200

    refactor(images): adjust home page, pregnancy, about me images

--- src/general/components/layouts/two-images.layout.tsx
+++ src/general/components/layouts/two-images.layout.tsx
@@ -42,17 +42,17 @@ export function TwoImagesLayout({ page, reverse }: TwoImagesLayoutProps) {
     <Page className="grid grid-cols-6 gap-4">
       {!reverse && textComponent}
       {/** images for lg+ **/}
-      <div className="flex-col items-center justify-start flex-grow hidden col-span-6 pl-8 mt-8 lg:flex lg:col-span-3 xl:col-span-3 2xl:col-span-3 lg:mt-0 xl:flex">
+      <div className="flex-col items-start justify-start flex-grow hidden col-span-6 pl-8 mt-8 lg:flex lg:col-span-3 xl:col-span-3 2xl:col-span-3 lg:mt-0 xl:flex">
         <CircleImage
           image={getImageRoute(page, 1)}
           border={ImageBorderThickness.small}
-          className="mx-auto -mt-16 xl:mt-0 xl:w-[400px] 2xl:w-[500px] md:relative md:top-[100px] md:right-[50px] md:w-[250px] lg:w-[350px]"
+          className="mx-auto -mt-16 xl:mt-0 xl:w-[400px] 2xl:w-[500px] md:relative md:top-[20px] md:right-[50px] md:w-[250px] lg:w-[350px]"
         />
 
         <DoorImage
           image={getImageRoute(page, 2)}
           border={ImageBorderThickness.small}
-          className="mx-auto -mt-12 xl:w-[400px] 2xl:w-[500px] md:relative md:top-[80px] md:left-[50px] md:w-[300px] lg:w-[350px]"
+          className="mx-auto -mt-12 xl:w-[400px] 2xl:w-[500px] md:relative md:top-[0px] md:left-[50px] md:w-[300px] lg:w-[350px]"
         />
       </div>
       {!reverse && smallImages}
