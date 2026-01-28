e9685b57658a0bfe165953efba5865f5992dc518
Tobias Winkler <7114819+Tschuck@users.noreply.github.com>
2026-01-28 20:31:59 +0100

    fix: update server to latest fastify version

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import fastify from "fastify";
-import fastifyStatic from "fastify-static";
+import fastifyStatic from "@fastify/static";
 import path from "path";
 import process from "process";
 
@@ -7,7 +7,6 @@ const server = fastify({
   disableRequestLogging: true,
   logger: {
     level: "debug",
-    prettyPrint: true,
     base: {
       pid: process.pid,
       hostname: "SERVER",
@@ -19,7 +18,7 @@ server.register(fastifyStatic, {
   root: path.resolve("./app"),
 });
 
-server.listen(1990, "0.0.0.0", (err) => {
+server.listen({ port: 1990, host: "0.0.0.0" }, (err) => {
   if (err) {
     server.log.error(err.message);
     process.exit(1);
